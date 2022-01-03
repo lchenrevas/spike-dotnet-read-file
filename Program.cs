1e8972805221f7d37fbbc80e0963d2dcb03dd5b4
Le Chen <le.chen@revas.io>
2022-01-03 15:36:38 +0100

    feat: add api that reverts string in file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapPost("/upload",
-async Task<IResult>(HttpRequest request) =>
+async Task<IResult> (HttpRequest request) =>
 {
     if (!request.HasFormContentType)
         return Results.BadRequest();
@@ -14,10 +16,18 @@ async Task<IResult>(HttpRequest request) =>
         return Results.BadRequest();
 
     await using var stream = formFile.OpenReadStream();
-
     var reader = new StreamReader(stream);
-    var text = await reader.ReadToEndAsync();
 
-    return Results.Ok(text);
+    var text = await reader.ReadToEndAsync();
+    char[] charArray = text.ToCharArray();
+    Array.Reverse(charArray);
+    var reverse = new string(charArray);
+
+    // convert string to stream
+    byte[] byteArray = Encoding.ASCII.GetBytes(reverse);
+    MemoryStream outstream = new MemoryStream(byteArray);
+
+    return Results.Stream(outstream, "text/plain");
 });
+
 app.Run();
